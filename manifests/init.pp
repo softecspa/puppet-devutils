e8b91cf4f93f6b0122b4e78605322eb13a1b2c37
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-05-15 14:54:42 +0000

    styleguide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,19 @@
 class devutils {
 
-    package { 
-        "autoconf":         ensure  => present;
-        "automake1.9":      ensure  => present;
-        "autotools-dev":    ensure  => present;
-        "binutils":         ensure  => present;
-        "build-essential":  ensure  => present;
-        "cpp":              ensure  => present;
-        "gcc":              ensure  => present;
-        "g++":              ensure  => present;
-        "libc6-dev":        ensure  => present;
-        "libtool":          ensure  => present;
-        "linux-libc-dev":   ensure  => present;
-        "m4":               ensure  => present;
-        "make":             ensure  => present;
-        "manpages-dev":     ensure  => present;
-    }
+  package {
+    'autoconf':         ensure  => present;
+    'automake1.9':      ensure  => present;
+    'autotools-dev':    ensure  => present;
+    'binutils':         ensure  => present;
+    'build-essential':  ensure  => present;
+    'cpp':              ensure  => present;
+    'gcc':              ensure  => present;
+    'g++':              ensure  => present;
+    'libc6-dev':        ensure  => present;
+    'libtool':          ensure  => present;
+    'linux-libc-dev':   ensure  => present;
+    'm4':               ensure  => present;
+    'make':             ensure  => present;
+    'manpages-dev':     ensure  => present;
+  }
 }
